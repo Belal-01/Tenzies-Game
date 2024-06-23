0fc856486aee3616c456114d82b40959bf476258
Belal-01 <belalkhobieh343@gmail.com>
2024-06-23 18:15:06 +0300

    styling the Tenzies game

--- Tenzies game/src/App.jsx
+++ Tenzies game/src/App.jsx
@@ -1,12 +1,14 @@
-
+import '/App.css'
 
 const App = () => {
   return (
     <>
-      <div className="black-container">
-        <div className="white-container">
+      <div className="container__black">
+        <div className="container__white">
           <h1>Tenzies</h1>
-          <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+          <p>
+            Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
+          </p>
 
 
 
