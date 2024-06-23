80928fcf16d4487ba7e62ca7a99849a950603c2f
Belal-01 <belalkhobieh343@gmail.com>
2024-06-23 16:47:36 +0300

    creating the Html and preapering sass for the Game

--- Tenzies game/src/App.jsx
+++ Tenzies game/src/App.jsx
@@ -1,1 +1,21 @@
 
+
+const App = () => {
+  return (
+    <>
+      <div className="black-container">
+        <div className="white-container">
+          <h1>Tenzies</h1>
+          <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+
+
+
+          <button>Roll</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
+
